82c5a5d6d8889d59c161fb58987580e06e511f80
Kesavan P <pkesavan710@gmail.com>
2025-04-20 13:24:22 +0530

    prime commited

--- prime.c
+++ prime.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-#include<string.h>
-#include <ctype.h>
 
 int main() {
-    char s,a[100];
-    printf("Enter the string\n");
-    scanf("%[^\n]s",a);
-    int len=strlen(a),i;
-    int vowel=0,con=0;
-    for(i=0;i<len;i++)
-    {
-        s=a[i];
-        tolower(s);
-        if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u' )
-            vowel++;
-        else
-            con++;
-    }
-    printf("Number of vowels=%d\n",vowel);
-    printf("Number of consonants=%d\n",con);
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
+    int i, j, count = 0;
+
+    for (i = 2; i < n; i++) {
+        count = 0;
+        for (j = 1; j <= i; j++) {
+            if (i % j == 0)
+                count++;
+        }
+        if (count == 2)
+            printf("%d\t", i);
+    }
+
+    return 0;
 }
